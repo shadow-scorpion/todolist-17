1eecc21d1d7e89f007366532718f9cd12d186ffc
Alex <secret.teamplayer@gmail.com>
2025-12-21 19:25:09 +0200

    Added optional property captcha

--- src/features/auth/lib/schemas/loginSchema.ts
+++ src/features/auth/lib/schemas/loginSchema.ts
@@ -7,6 +7,7 @@ export const loginSchema = z.object({
     .min(1, { error: "Password is required" })
     .min(3, { error: "Password must be at least 3 characters long" }),
   rememberMe: z.boolean().optional(),
+  captcha: z.string().optional()
 })
 
 export type LoginInputs = z.infer<typeof loginSchema>
