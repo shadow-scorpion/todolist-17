744e960d603821e38417fb4e2e22b552802210ea
Alex <secret.teamplayer@gmail.com>
2025-12-21 19:23:54 +0200

    Created auth-slice

---
+++ src/features/auth/model/auth-slice.ts
@@ -0,0 +1,44 @@
+import { createAppSlice, handleServerAppError, handleServerNetworkError } from "@/common/utils"
+import { LoginInputs } from "@/features/auth/lib/schemas"
+import { setAppStatusAC } from "@/app/app-slice.ts"
+import { authApi } from "@/features/auth/api/authApi.ts"
+import { ResultCode } from "@/common/enums"
+
+export const authSlice = createAppSlice({
+  name: "auth",
+  initialState: {
+    isLoggedIn: false,
+  },
+  selectors: {
+    selectIsLoggedIn: (state) => state.isLoggedIn,
+  },
+  reducers: (create) => ({
+    loginTC: create.asyncThunk(
+      async (data: LoginInputs, { dispatch, rejectWithValue }) => {
+        try {
+          dispatch(setAppStatusAC({ status: "loading" }))
+          const res = await authApi.login(data)
+          if (res.data.resultCode === ResultCode.Success) {
+            dispatch(setAppStatusAC({ status: "succeeded" }))
+            return { isLoggedIn: true }
+          } else {
+            handleServerAppError(res.data, dispatch)
+            return rejectWithValue(null)
+          }
+        } catch (e) {
+          handleServerNetworkError(dispatch, e)
+          return rejectWithValue(null)
+        }
+      },
+      {
+        fulfilled: (state, action) => {
+          state.isLoggedIn = action.payload.isLoggedIn
+        },
+      },
+    ),
+  }),
+})
+
+export const { selectIsLoggedIn } = authSlice.selectors
+export const { loginTC } = authSlice.actions
+export const authReducer = authSlice.reducer
