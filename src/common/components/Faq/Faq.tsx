cfb37949726eb44a025e709a906d2fbe39fe8be0
Alex <secret.teamplayer@gmail.com>
2025-12-25 18:15:48 +0200

    FAQ component

---
+++ src/common/components/Faq/Faq.tsx
@@ -0,0 +1,26 @@
+
+export const Faq = () => {
+
+  return (
+    <div>
+      <h1>Faq</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae doloremque earum excepturi iure laborum
+        mollitia nisi nobis non officiis, praesentium quia saepe, sed similique tempora unde? Alias aliquam animi
+        aperiam architecto autem consectetur earum et eum expedita explicabo fugit in inventore laborum, laudantium
+        libero magnam minima nobis officia porro praesentium provident qui quis recusandae rem repellat saepe sequi sunt
+        tempore vero voluptates. Deleniti dolor facilis inventore nostrum quibusdam quo, unde vitae! Consectetur,
+        delectus eaque earum facere impedit iste possimus reprehenderit. Aperiam asperiores assumenda consequatur,
+        delectus dicta dolor et excepturi iusto laborum, neque nostrum, optio placeat provident quae qui repudiandae
+        voluptate voluptates. Consequuntur fuga laudantium omnis possimus velit? Alias aspernatur at dignissimos dolor
+        est eveniet ex, id impedit modi neque nobis nostrum pariatur perferendis placeat quas quis quo recusandae
+        reiciendis reprehenderit suscipit ullam unde velit voluptates! Alias animi dignissimos fugiat mollitia odit
+        optio praesentium. Animi, at corporis debitis dicta facilis, illo ipsa iusto maxime nihil pariatur, quibusdam
+        unde. Deleniti minus quasi quisquam totam. Aperiam consequatur cumque delectus deleniti dignissimos earum eius
+        error explicabo facere, laudantium nulla obcaecati omnis quidem temporibus unde ut voluptate? Aperiam ipsum
+        laborum ut voluptates. Adipisci et iste minus nulla odio. Accusamus error fugiat non quod sint tenetur!
+      </p>
+    </div>
+  )
+}
+
