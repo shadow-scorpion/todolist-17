dde347712864a798ad3ec232dcd67b05b55ab99b
Alex <secret.teamplayer@gmail.com>
2025-12-26 16:53:42 +0200

    Expected authApi

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -3,7 +3,13 @@ import { LoginInputs } from "@/features/auth/lib/schemas"
 import { BaseResponse } from "@/common/types"
 
 export const authApi = {
+  fetchMe() {
+    return instance.get<BaseResponse<{ id: number; email: string; login: string }>>("auth/me")
+  },
   login(payload: LoginInputs) {
     return instance.post<BaseResponse<{ userId: number; token: string }>>("auth/login", payload)
+  },
+  logout() {
+    return instance.delete<BaseResponse>("auth/login")
   },
 }
