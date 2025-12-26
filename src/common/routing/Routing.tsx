8b86005220824bf7da4492e86319411a53b32820
Alex <secret.teamplayer@gmail.com>
2025-12-26 16:51:37 +0200

    Improved Routing

--- src/common/routing/Routing.tsx
+++ src/common/routing/Routing.tsx
@@ -1,18 +1,29 @@
 import { Main } from "@/app/Main"
-import { PageNotFound } from "@/common/components"
+import { PageNotFound, Faq } from "@/common/components"
 import { Login } from "@/features/auth/ui/Login/Login"
 import { Route, Routes } from "react-router"
+import { ProtectedRoute } from "@/common/components/ProtectedRoute/ProtectedRoute.tsx"
+import { useAppSelector } from "@/common/hooks"
+import { selectIsLoggedIn } from "@/features/auth/model/auth-slice.ts"
 
 export const Path = {
   Main: "/",
-  Login: "login",
+  Login: "/login",
+  Faq: "/faq",
   NotFound: "*",
 } as const
 
-export const Routing = () => (
-  <Routes>
-    <Route path={Path.Main} element={<Main />} />
-    <Route path={Path.Login} element={<Login />} />
+export const Routing = () => {
+  const isLoggedIn = useAppSelector(selectIsLoggedIn)
+
+  return <Routes>
+    <Route path={Path.Main} element={<ProtectedRoute redirectPath={Path.Login} isAllowed={isLoggedIn}><Main /></ProtectedRoute>} />
+    <Route path={Path.Faq} element={<ProtectedRoute redirectPath={Path.Login} isAllowed={isLoggedIn}><Faq /></ProtectedRoute>} />
+    <Route path={Path.Login} element={
+      <ProtectedRoute redirectPath={Path.Main} isAllowed={!isLoggedIn}>
+        <Login />
+      </ProtectedRoute>}
+    />
     <Route path={Path.NotFound} element={<PageNotFound />} />
   </Routes>
-)
+}
