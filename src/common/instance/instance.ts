b9bae4790356bdae8d2b94497114b989dae0d6e5
Alex <secret.teamplayer@gmail.com>
2025-12-26 16:54:52 +0200

    Added interceptors for all requests

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -1,9 +1,17 @@
 import axios from "axios"
+import { AUTH_TOKEN } from "@/common/constants"
 
 export const instance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
-    Authorization: `Bearer ${import.meta.env.VITE_AUTH_TOKEN}`,
     "API-KEY": import.meta.env.VITE_API_KEY,
   },
+})
+
+instance.interceptors.request.use(function (config) {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
 })
